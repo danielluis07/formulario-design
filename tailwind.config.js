e71186f0f23d13137ad156369dbf72b98c79b9be
danielluis07 <bombdaniel@hotmail.com>
2023-06-16 11:16:33 -0300

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        tomato: "hsl(4, 100%, 67%)",
+        darkslategrey: "hsl(234, 29%, 20%)",
+        charcoalgrey: "hsl(235, 18%, 26%)",
+        maingrey: "hsl(231, 7%, 60%)",
+        mainwhite: "hsl(0, 0%, 100%)",
+        lightorange: "#FF6742",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
       },
     },
   },
   plugins: [],
-}
+};
